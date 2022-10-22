86ba41372b1b81977496789c4e154a1ee67b5b61
raycaohmu <stat-caolei@localhost.localdomain>
2022-10-22 19:49:17 +0800

    first commit

---
+++ datasets/camel_data.py
@@ -0,0 +1,66 @@
+import random
+import torch
+import pandas as pd
+from pathlib import Path
+
+import torch.utils.data as data
+from torch.utils.data import dataloader
+
+
+class CamelData(data.Dataset):
+    def __init__(self, dataset_cfg=None,
+                 state=None):
+        # Set all input args as attributes
+        self.__dict__.update(locals())
+        self.dataset_cfg = dataset_cfg
+
+        #---->data and label
+        self.nfolds = self.dataset_cfg.nfold
+        self.fold = self.dataset_cfg.fold
+        self.feature_dir = self.dataset_cfg.data_dir
+        self.csv_dir = self.dataset_cfg.label_dir + f'fold{self.fold}.csv'
+        self.slide_data = pd.read_csv(self.csv_dir, index_col=0)
+
+        #---->order
+        self.shuffle = self.dataset_cfg.data_shuffle
+
+        #---->split dataset
+        if state == 'train':
+            self.data = self.slide_data.loc[:, 'train'].dropna()
+            self.label = self.slide_data.loc[:, 'train_label'].dropna()
+        if state == 'val':
+            self.data = self.slide_data.loc[:, 'val'].dropna()
+            self.label = self.slide_data.loc[:, 'val_label'].dropna()
+        if state == 'test':
+            self.data = self.slide_data.loc[:, 'test'].dropna()
+            self.label = self.slide_data.loc[:, 'test_label'].dropna()
+
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        slide_id = self.data[idx]
+        label = int(self.label[idx])
+        full_path = Path(self.feature_dir) / f'{slide_id}.pt'
+        features = torch.load(full_path)[:, :-2]
+
+        #----> shuffle
+        if self.shuffle == True:
+            index = [x for x in range(features.shape[0])]
+            random.shuffle(index)
+            features = features[index]
+
+
+        return features, label
+
+
+if __name__ == "__main__":
+    import yaml
+    from addict import Dict
+
+    with open("../Camelyon/TransMIL.yaml", mode="r") as file:
+        yml = yaml.load(file, Loader=yaml.Loader)
+        cfg = Dict(yml)
+    dataset = CamelData(dataset_cfg=cfg, state="train")
+    import ipdb;ipdb.set_trace()
